b8dc53e4e47823db1f59dcde3fbbfe921086a31d
grayrattus <arturdziedziczak0@gmail.com>
2020-11-19 20:40:35 +0100

    Add even more stuff

--- zad1.m
+++ zad1.m
@@ -3,32 +3,22 @@ function zad1()
     deltaT=[-1500, -1000, -300,-50, -1, 1, 20, 50,200,400,1000,2000];
     hMatrix=[178, 176, 168, 161,160,160,160.2, 161, 165, 168, 174, 179];
 
-    % wykresy_dla_tabeli(0.1);
-    % wykresy_dla_tabeli(0.001);
-    % wykresy_dla_tabeli_z_ruchomym_h();
-    % wykresy_dla_tabeli_z_ruchomym_h_dla_roznego_stopnia_wielomianu(5);
-    % wykresy_dla_tabeli_z_ruchomym_h_dla_roznego_stopnia_wielomianu(8);
-    % obliczanie_calki_simpsonem();
-    % wykresy_modelu_aproksymujacego();
+    Tb0 = [1200, 800, 1100, 1200, 800, 1100, 1100, 1100, 1100, 1100];
+    Tw0 = [25, 25, 70, 25, 25, 70, 70, 70, 70, 70];
+    Mw = [2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 5, 10, 2.5, 2.5];
+    czasy = [3, 3, 3, 5, 5, 2, 2, 2, 4, 5];
+    Tbk = [107.7, 79.1, 142.1, 105.7, 78.2, 150.1, 116.6, 99.1, 141.2, 140.9];
+    Twk = [105.1, 78.0, 139.1, 105.5, 78.1, 138.2, 105.1, 88.1, 139.8, 140.1];
+
+    wykresy_dla_tabeli(0.1);
+    wykresy_dla_tabeli(0.01);
+    wykresy_dla_tabeli(0.001);
+    wykresy_dla_tabeli_z_ruchomym_h();
+    wykresy_dla_tabeli_z_ruchomym_h_dla_roznego_stopnia_wielomianu(5);
+    wykresy_dla_tabeli_z_ruchomym_h_dla_roznego_stopnia_wielomianu(8);
+    obliczanie_calki_simpsonem();
+    wykresy_modelu_aproksymujacego();
     newton_raphson();
-
-    function zadanie_4()
-        ilosc_pretow_do_schlodzenia = 1000000;
-        czas_do_schlodzenia = 24 * 60 * 60;
-
-        czas_wymiany_zbiornikow = 30;
-        
-        czas_chlodzenia_zbiornika_ze_125_do_25_stopni = masa_wody * 0.15 * 60 * 60;
-
-        koszt_wyprodukowania_zbiornika = 100 * masa_wody; % PLN
-        koszt_oleju_do_wypelnienia_zbiornika = 20 * masa_wody; %PLN
-        ilosc_pretow_do_zubozenia_oleju = 2000;
-
-        % for sekunda: 1:1:czas_do_schlodzenia
-        %     
-        % end
-
-    end
 
     function newton_raphson()
         krok=0.01;
@@ -67,9 +57,15 @@ function zad1()
             
         fig=figure('Renderer', 'painters', 'Position', wielkosc_wykresu)
         plot(masy_wody, temperatury, 'o');
-        title(sprintf('Wykres temperatur dla zmiany w masie wody. Wymagany czas schłodzenia: %d s \n do temperatury: %d z ostateczną masą wody: %0.3f ',zadany_czas, wymagana_temperatura, masy_wody(length(masy_wody))));
-        xlabel('Masa wody [kg]');
-        ylabel('Temperatura [stopnie celsiusza]');
+        title(sprintf('Wykres temperatur dla zmiany w masie wody\nWymagany czas schlodzenia: %0.3f s \n do temperatury: %d $[^\\circ C]$ z ostateczna masa wody: %0.3f [kg] ',zadany_czas, wymagana_temperatura, masy_wody(length(masy_wody))), 'interpreter', 'latex');
+        annotation('textbox',[.15 .1 .8 .8],'interpreter', 'latex', 'String',{
+            sprintf('$cb(%0.3f)[\\frac{J}{kg*K}]$', cb),
+            sprintf('$A(%0.5f)[m^2]$', A),
+            sprintf('$cw(%0.5f)[\\frac{J}{kg*K}]$', cw),
+            sprintf('$mb(%0.2f)[kg]$', mb),
+        },'EdgeColor','none');
+        xlabel('Masa wody[kg]','interpreter', 'latex');
+        ylabel('$Temperatura\:preta[^\circ C]$','interpreter', 'latex');
         legend('Temperatura pręta');
         saveas(fig,sprintf('NewtonRaphson.png'));
 
@@ -100,8 +96,8 @@ function zad1()
 
         title(sprintf('Całka metodą simpsona pomiędzy funkcjami sklejanmi 3 stopnia a metodą kwadratów. \n Wartość całki: %.3f', wartosc_calki));
         shade(di,wartosci_kwadratow, di, wartosci_sklejanych, 'FillType', [1, 2; 2, 1]);
-        xlabel('DeltaT[stopnie Cel]');
-        ylabel('h[W*m^-2]');
+        xlabel('$\Delta T[^\circ C]$','interpreter', 'latex');
+        ylabel('$h[W*m^{-2}]$','interpreter', 'latex');
         legend('Metoda kwadratów', 'Funkcje sklejane 3 stopnia');
         saveas(fig,sprintf('Metoda_simpsona.png'));
 
@@ -137,8 +133,8 @@ function zad1()
         end
         legend(labele_wielomianu);
         title(sprintf('Wykres aproksymacji najmniejszych kwadratów dla różnych stopni wielomianów'));
-        xlabel('DeltaT[stopnie Cel]');
-        ylabel('h[W*m^-2]');
+        xlabel('$\Delta T[^\circ C]$','interpreter', 'latex');
+        ylabel('$h[W*m^{-2}]$','interpreter', 'latex');
         saveas(fig,sprintf('Stopnie_wielomianu_%d.png',stopnie_wielomianu));
         hold off;
 
@@ -158,12 +154,6 @@ function zad1()
         a=aproksymacja_najmniejszych_kwadratow(deltaT, hMatrix,5);
         ABCs = oblicz_wspolczynniki_fn_sklejanych_3_stopnia(deltaT, hMatrix);
 
-        Tb0 = [1200, 800, 1100, 1200, 800, 1100, 1100, 1100, 1100, 1100];
-        Tw0 = [25, 25, 70, 25, 25, 70, 70, 70, 70, 70];
-        Mw = [2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 5, 10, 2.5, 2.5];
-        czasy = [3, 3, 3, 5, 5, 2, 2, 2, 4, 5];
-        Tbk = [107.7, 79.1, 142.1, 105.7, 78.2, 150.1, 116.6, 99.1, 141.2, 140.9];
-        Twk = [105.1, 78.0, 139.1, 105.5, 78.1, 138.2, 105.1, 88.1, 139.8, 140.1];
         h=160; % współczynnik przewodnictwa cieplnego
 
         for nr_pomiaru = 1:length(Tb0)
@@ -207,9 +197,15 @@ function zad1()
 
             plot(t, y_kwadraty(1,:), t, y_kwadraty(2,:), t, y_funkcje_3_stopnia(1,:), t, y_funkcje_3_stopnia(2,:), t, y_funkcje_1_stopnia(1,:), t, y_funkcje_1_stopnia(2,:), t, y_stale_h(1, :), t, y_stale_h(2,:));
 
-            title(sprintf('Wykres dla Tb0(%d),Tw0(%d),czasu(%0.0f),Mw(%d),krok(%0.3f)',Tb0(nr_pomiaru), Tw0(nr_pomiaru),czasy(nr_pomiaru), Mw(nr_pomiaru), krok));
-            xlabel('Czas [s]');
-            ylabel('Temperatura [stopnie celsiusza]');
+            title(sprintf('$Wykres\\:dla\\:Tb0(%d)[^\\circ C],Tw0(%d)[^\\circ C],czasu(%0.0f)[s],Mw(%0.1f)[kg],krok(%0.3f)$',Tb0(nr_pomiaru), Tw0(nr_pomiaru),czasy(nr_pomiaru), Mw(nr_pomiaru), krok), 'interpreter', 'latex');
+            annotation('textbox',[.15 .1 .8 .8],'interpreter', 'latex', 'String',{
+                sprintf('$cb(%0.3f)[\\frac{J}{kg*K}]$', cb),
+                sprintf('$A(%0.5f)[m^2]$', A),
+                sprintf('$cw(%0.5f)[\\frac{J}{kg*K}]$', cw),
+                sprintf('$mb(%0.2f)[kg]$', mb),
+            },'EdgeColor','none');
+            xlabel('Czas[s]');
+            ylabel('$Temperatura[^\circ C]$','interpreter', 'latex');
             legenda={
                 'Temp Pręta Najmniejsze kwadraty'
                 'Temp Oleju Najmniejsze kwadraty'
@@ -234,17 +230,11 @@ function zad1()
 
         a=aproksymacja_najmniejszych_kwadratow(deltaT, hMatrix,5);
 
-        cb=3.85; % pojemnośc cieplna metalu pręta
-        A=0.0109; % powierzchnia pręta
-        cw=4.1813; % pojemność cieplna 
-        h=160; % współczynnik przewodnictwa cieplnego
-        mb=0.2; % masa pręta
-
         for i=1:length(di)
             dl(i) = obliczanie_wielomianu(deltaT, a, di(i));
         end
 
-        fig=figure('Renderer', 'painters', 'Position', wielkosc_wykresu)
+        fig=figure('Renderer', 'painters', 'Position', wielkosc_wykresu);
         hold on;
         plot(deltaT, hMatrix, 'o', di, dl);
         ABCs = oblicz_wspolczynniki_fn_sklejanych_3_stopnia(deltaT, hMatrix);
@@ -259,8 +249,8 @@ function zad1()
         plot(di, dl, 'g');
 
         title(sprintf('Wykres dla współczynnika przewodnictwa cieplnego'));
-        xlabel('DeltaT[stopnie Cel]');
-        ylabel('h[W*m^-2]');
+        xlabel('$\Delta T[^\circ C]$','interpreter', 'latex');
+        ylabel('$h[W*m^{-2}]$','interpreter', 'latex');
         legend('Charakterystyka pomiarowa', 'Aproksymacja najmniejszych kwadratów', 'Interpolacja funkcjami sklejanymi 3 stopnia', 'Interpolacja funkcjami 1 stopnia');
         saveas(fig,sprintf('Charakterystyka_ruchomego_h.png'));
 
@@ -269,13 +259,6 @@ function zad1()
     end
 
     function wykresy_dla_tabeli(krok)
-        Tb0 = [1200, 800, 1100, 1200, 800, 1100, 1100, 1100, 1100, 1100];
-        Tw0 = [25, 25, 70, 25, 25, 70, 70, 70, 70, 70];
-        Mw = [2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 5, 10, 2.5, 2.5];
-        czasy = [3, 3, 3, 5, 5, 2, 2, 2, 4, 5];
-        Tbk = [107.7, 79.1, 142.1, 105.7, 78.2, 150.1, 116.6, 99.1, 141.2, 140.9];
-        Twk = [105.1, 78.0, 139.1, 105.5, 78.1, 138.2, 105.1, 88.1, 139.8, 140.1];
-
         bledy_euler_Tb = [];
         bledy_euler_Tw = [];
 
@@ -317,9 +300,16 @@ function zad1()
             bledy_ulepszony_euler_Tw = [bledy_ulepszony_euler_Tw , blad_wzgledny(Twk(nr_pomiaru), ostatni_wynik(2))];
 
             plot(t, y(1,:), t, y(2,:));
-            title(sprintf('Wykres dla Tb0(%d),Tw0(%d),czasu(%0.0f),Mw(%d),krok(%0.3f)',Tb0(nr_pomiaru), Tw0(nr_pomiaru),czasy(nr_pomiaru), Mw(nr_pomiaru), krok));
-            xlabel('Czas [s]');
-            ylabel('Temperatura [stopnie celsiusza]');
+            annotation('textbox',[.15 .1 .8 .8],'interpreter', 'latex', 'String',{
+                sprintf('$cb(%0.3f)[\\frac{J}{kg*K}]$', cb),
+                sprintf('$A(%0.5f)[m^2]$', A),
+                sprintf('$cw(%0.5f)[\\frac{J}{kg*K}]$', cw),
+                sprintf('$h(%0.2f)[W*m^{-2}]$', h),
+                sprintf('$mb(%0.2f)[kg]$', mb),
+            },'EdgeColor','none')
+            title(sprintf('$Wykres\\:dla\\:Tb0(%d)[^\\circ C],Tw0(%d)[^\\circ C],czasu(%0.0f)[s],Mw(%0.1f)[kg],krok(%0.3f)$',Tb0(nr_pomiaru), Tw0(nr_pomiaru),czasy(nr_pomiaru), Mw(nr_pomiaru), krok), 'interpreter', 'latex');
+            xlabel('Czas[s]');
+            ylabel('$Temperatura[^\circ C]$','interpreter', 'latex');
             legend('Euler: Temperatura pręta','Euler: Temperatura wody', 'Ulepszony Euler: Temperatura pręta', 'Ulepszony Euler: Temperatura wody');
             saveas(fig,sprintf('Pomiar_%d_krok_%d.png',nr_pomiaru, krok * 1000));
 
